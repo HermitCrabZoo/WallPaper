d142bbb2b5ab06ca6f005f435af68eaf793e816e
HermitCrabZoo <695863762@qq.com>
2019-12-17 22:07:30 +0800

    修复在站点更新后无法下载的问题

--- AwesomeWallpaper.py
+++ AwesomeWallpaper.py
@@ -16,12 +16,12 @@ import copy
 logger = Logger.get("AwesomeWallpaper")
 
 
-headers = {"Referer": "https://alpha.wallhaven.cc/", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"}
+headers = {"Referer": "https://wallhaven.cc/", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"}
 
 executor = None
 
 # 主页url
-home_url = "https://alpha.wallhaven.cc"
+home_url = "https://wallhaven.cc"
 # 登录url
 login_url = home_url + "/auth/login"
 # 会话
@@ -77,7 +77,7 @@ def addressing(previews):
             response = session.get(url, timeout=timeout)
             img_url = Q(response.text)("#wallpaper").attr("src")
             if img_url:
-                downloads.append("https:{}".format(img_url))
+                downloads.append(img_url)
             else:
                 logger.info("未找到下载图片的url：{}".format(url))
         except Exception as e:
@@ -177,25 +177,28 @@ def control():
     # 分辨率
     resolutions = ["1280x720", "1280x800", "1280x960", "1280x1024", "1600x900", "1600x1000", "1600x1200", "1600x1280", "1920x1080", "1920x1200", "1920x1440",
                    "1920x1536", "2560x1440", "2560x1600", "2560x1920", "2560x2048", "3840x2160", "3840x2400", "3840x2880", "3840x3072"]
+    # 并发数
+    parallels = list(range(1, os.cpu_count() * 2 + 1))
+    parallel_metavar = "{"+",".join(map(str, parallels[:2]+["..."]+parallels[-2:]))+"}" if len(parallels) > 4 else "{"+",".join(map(str, parallels))+"}"
 
     # 校验参数
     parser = ArgumentParser(description="Awesome Wallpaper提取", epilog="此致，敬礼！")
-    parser.add_argument("-dir", type=AccessiblePath(), dest="dir", metavar="dir", help="保存的目录，必选。", required=True)
+    parser.add_argument("-dir", type=AccessiblePath(), dest="dir", metavar="<dir>", help="保存的目录，必选。", required=True)
     parser.add_argument("-mode", type=str, dest="mode", choices=["search", "random", "toplist", "latest"], help="搜索模式，默认random，仅search模式支持其他过滤条件。", default="random")
-    parser.add_argument("-q", "--query", type=str, dest="query", metavar="query", help="搜索关键字，默认空。", default="")
+    parser.add_argument("-q", "--query", type=str, dest="query", metavar="<query>", help="搜索关键字，默认空。", default="")
     parser.add_argument("-c", "--category", type=str, dest="category", metavar="{"+",".join(categories)+"}", choices=categories, help="分类：'普通'、'动漫'、'人物'；(1：包含，0：不包含)，可叠加，默认110。", default="111")
     parser.add_argument("-p", "--purity", type=str, dest="purity", metavar="{"+",".join(puritys)+"}", choices=puritys, help="内容风格：'科幻'、'素描'、'重口'；(1：包含，0：不包含)，可叠加，默认110。", default="110")
     parser.add_argument("-s", "--sort", type=str, dest="sort", choices=sortings, help="排序，默认relevance。", default="relevance")
     parser.add_argument("-o", "--order", type=str, dest="order", choices=orders, help="排序规则，默认desc。", default="desc")
-    parser.add_argument("-r", "--resolutions", type=str, dest="resolutions", metavar="resolution", choices=resolutions, help="分辨率，默认3840x2160(当mode为search时该参数才有效)。", default=["3840x2160"], nargs="*")
-    parser.add_argument("-f", "--from", type=Int(1), dest="from", metavar="from", help="起始页，默认1。", default=1)
-    parser.add_argument("-t", "--to", type=Int(1), dest="to", metavar="to", help="结束页，默认1。", default=1)
-    parser.add_argument("-timeout", type=Int(), dest="timeout", metavar="timeout", help="下载超时(s)，默认120。", default=120)
-    parser.add_argument("-times", type=Int(), dest="times", metavar="times", help="下载失败次数，默认5。", default=5)
-    parser.add_argument("-parallel", type=Int(1), dest="parallel", metavar="parallel", choices=range(1, os.cpu_count()+1), help="并发数,默认1。", default=1)
-    parser.add_argument("-limit", type=Int(1), dest="limit", metavar="limit", help="限制下载的个数，默认无限个。", default=sys.maxsize)
-    parser.add_argument("-user", type=str, dest="user", metavar="user", help="用户名。")
-    parser.add_argument("-pwd", type=str, dest="pwd", metavar="pwd", help="密码。")
+    parser.add_argument("-r", "--resolutions", type=str, dest="resolutions", metavar="<resolution>", choices=resolutions, help="分辨率，默认3840x2160(当mode为search时该参数才有效)。", default=["3840x2160"], nargs="*")
+    parser.add_argument("-f", "--from", type=Int(1), dest="from", metavar="<from>", help="起始页，默认1。", default=1)
+    parser.add_argument("-t", "--to", type=Int(1), dest="to", metavar="<to>", help="结束页，默认1。", default=1)
+    parser.add_argument("-timeout", type=Int(), dest="timeout", metavar="<timeout>", help="下载超时(s)，默认120。", default=120)
+    parser.add_argument("-times", type=Int(), dest="times", metavar="<times>", help="下载失败次数，默认5。", default=5)
+    parser.add_argument("-parallel", type=Int(1), dest="parallel", metavar=parallel_metavar, choices=parallels, help="并发数,默认1。", default=1)
+    parser.add_argument("-limit", type=Int(1), dest="limit", metavar="<limit>", help="限制下载的个数，默认无限个。", default=sys.maxsize)
+    parser.add_argument("-user", type=str, dest="user", metavar="<user>", help="用户名。")
+    parser.add_argument("-pwd", type=str, dest="pwd", metavar="<pwd>", help="密码。")
     # parser.add_argument("-interval", type=Float(), dest="interval", metavar="interval", help="下载间隔s，默认0。", default=0)
     args = parser.parse_args()
     copied = copy.deepcopy(args)
